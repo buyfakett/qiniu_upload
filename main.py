db1da393c7c8cdd2654611116c7995c7f5eba174
buyfakett <buyfakett@vip.qq.com>
2023-12-25 16:32:22 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from qiniu_util import QiniuFunction
+
+if len(sys.argv) != 3:
+    print('传参数量不对')
+    exit(1)
+else:
+    local_file = sys.argv[1]
+    remote_file = sys.argv[2]
+    qiniu = QiniuFunction()
+    ret, info = QiniuFunction.upload_file(local_file, remote_file)
+    if info.status_code != 200:
+        print('上传失败')
+        exit(1)
+    else:
+        print('上传成功')
+        exit(0)
